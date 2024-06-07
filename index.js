a7c1e557442b56f4d6d97da7fa60b25d38d2c734
Bastian Gabrielli <gabriellidoren@gmail.com>
2024-06-07 12:29:43 -0400

    final commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/* Inicializando variable */
+var precio = 400000;
+
+/* Selector del precio inicial del artículo */
+var precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerText = precio;
+
+/* Selector del valor total del DOM */
+var cantidadSpan = document.querySelector("span.cantidad");
+
+/* Selector de la cantidad total pára inicializar la variable en 0 */
+var valorTotalSpan = document.querySelector(".valor-total");
