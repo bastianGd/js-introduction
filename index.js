8febbd339631c92f7371bcb5626448d6f767ea92
Bastian Gabrielli <gabriellidoren@gmail.com>
2024-06-08 13:54:47 -0400

    se arregla salto de linea involuntario en total

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 /* Inicializando variable */
-var precio = 400000;
+const precio = 400000;
 
 /* Selector del precio inicial del artículo */
 var precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerText = precio;
+precioSpan.innerHTML = precio;
 
 /* Selector del valor total del DOM */
 var cantidadSpan = document.querySelector("span.cantidad");
 
-/* Selector de la cantidad total pára inicializar la variable en 0 */
+/* Selector de la cantidad total*/
 var valorTotalSpan = document.querySelector(".valor-total");
